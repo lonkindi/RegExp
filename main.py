c3da119294f4089bbab4590631b3ef0b2a6a1901
lonkindi <legions.ru@gmail.com>
2020-04-13 09:43:15 +0500

    Final commit

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ with open("phonebook_raw.csv", encoding='utf-8') as f:
 # TODO 1: выполните пункты 1-3 ДЗ
 contacts_list_new = [header]
 patt_FIO = re.compile('[А-Я][а-я]*')
-patt_tel = re.compile('(8|\+7)(\s*|)(\(|)(\d{3})(\)|)(\s*|-|)(\d{3})(-|)(\d{2})(-|)(\d{2})(\s*(\(|)(доб\.)\s*(\d*)|)')
+patt_tel = re.compile('(8|\+7)(\s*|)(\(|)(\d{3})(\)|)(\s*|-|)(\d{3})(-|)(\d{2})(-|)(\d{2})((\s*)(\(|)(доб\.)\s*(\d*)|)(\)|)')
 for item in contacts_list:
     res_F = patt_FIO.findall(item[0])
     res_I = patt_FIO.findall(item[1])
@@ -29,7 +29,7 @@ for item in contacts_list:
       item[1] = res_I[0]
       item[2] = res_I[1]
     # print(type(item[5]))
-    res_tel = patt_tel.sub(r'+7(\4)\7-\9-\11 \14\15', item[5])
+    res_tel = patt_tel.sub(r'+7(\4)\7-\9-\11\13\15\16', item[5])
     item[5] = res_tel
     item[6] = item[6].lower()
 
@@ -53,7 +53,7 @@ for item in contacts_list:
 print(contacts_list_new)
 # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
-with open("phonebook.csv", "w", encoding='utf-8') as f:
+with open("phonebook.csv", "w", encoding='utf-8', newline='') as f:
     datawriter = csv.writer(f, delimiter=',')
     # Вместо contacts_list подставьте свой список
     datawriter.writerows(contacts_list_new)
